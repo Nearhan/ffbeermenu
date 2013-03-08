88d9709ad46c7a38455988c889592778c0b6f540
Farhan Syed <farhan.syed@threespot.com>
2013-03-08 18:18:32 -0500

    added new settings dir

---
+++ ffbeermenu/ffbeermenu/settings/prod.py
@@ -0,0 +1,16 @@
+from .base import *
+
+import dj_database_url
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+     ('Farhan Syed', 'nearhan@gmail.com'),
+)
+
+MANAGERS = ADMINS
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
