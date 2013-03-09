5e16c71ba1286fa07794cd61579a5e13dd82b875
Farhan Syed <farhan.syed@threespot.com>
2013-03-09 16:16:20 -0500

    added stuff to prod env

--- ffbeermenu/ffbeermenu/settings/prod.py
+++ ffbeermenu/ffbeermenu/settings/prod.py
@@ -6,11 +6,12 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-     ('Farhan Syed', 'nearhan@gmail.com'),
+    ('Farhan Syed', 'nearhan@gmail.com'),
 )
 
 MANAGERS = ADMINS
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
